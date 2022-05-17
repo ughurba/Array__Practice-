630bbe5466b04f4fce4049e7cc86e32c621d26db
ughurba <ughurba@edu.code.az>
2022-05-18 01:26:36 +0400

    nice ,very nice :)

--- app.js
+++ app.js
@@ -12,8 +12,22 @@ function squareTotal(arr){
         if(item % 2===0){
             item *=item
             result += item
-        }
+        };
     });
-    return result
+    return result;
 }
-console.log(squareTotal([4,2,3,5,6]))
+
+console.log(squareTotal([4,2,3,5,6]));
+
+
+
+// 3) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapan bir function yazin.
+
+let sum = (n) => {
+    if(n % 3 === 0 && n % 7 === 0){
+        console.log(`${n}  3-ce ve 7-e bolunur`)
+    }else{
+        console.log(`${n} 3-ce ve 7-e bolunmur`)
+    }
+}
+sum(42);
